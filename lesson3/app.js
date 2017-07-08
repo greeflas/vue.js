faccadb23fb4cc47417c6b833500487729cfea37
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-07-08 09:07:57 +0300

    Directive: v-once

--- lesson3/app.js
+++ lesson3/app.js
@@ -1,9 +1,11 @@
 var vm = new Vue({
+    el: '#app',
     data: {
-        test: 7
+        test: 7,
+        msg: 'test v-once directive'
     },
     created: function () {
-        // this is vm object
+        // `this` is vm object
         console.log('Value of test field: ' + this.test);
     }
 });

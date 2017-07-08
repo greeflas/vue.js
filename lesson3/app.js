38d5e6a2c48d07815d2a920181d06159e817851b
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-07-08 09:15:30 +0300

    Directive: v-bind for attributes

--- lesson3/app.js
+++ lesson3/app.js
@@ -2,8 +2,13 @@ var vm = new Vue({
     el: '#app',
     data: {
         test: 7,
+
         msg: 'test v-once directive',
-        rawHtmlContent: '<h1>This is raw HTML</h1>'
+
+        rawHtmlContent: '<h1>This is raw HTML</h1>',
+
+        dynamicId: 'btn-js',
+        isBtnActive: true
     },
     created: function () {
         // `this` is vm object

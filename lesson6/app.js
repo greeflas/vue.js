6546179830ed38893d78af711882ea9495a73c07
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-07-15 09:58:14 +0300

    Styles: v-bind:class

---
+++ lesson6/app.js
@@ -0,0 +1,13 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        isActive: true,
+
+        classObject: {
+            active: true
+            // has-error: function () {}
+        },
+
+        activeClass: 'active'
+    }
+});

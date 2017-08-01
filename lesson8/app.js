19f24b6534700d48f03c6f5314fc62232b458e3a
greeflas <vldmr.kuprienko@gmail.com>
2017-08-01 16:22:48 +0300

    Directive: v-for

---
+++ lesson8/app.js
@@ -0,0 +1,15 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            {label: 'first'},
+            {label: 'second'},
+            {label: 'third'}
+        ],
+        user: {
+            username: 'greeflas',
+            firstName: 'Vladimir',
+            lastName: 'Kuprienko'
+        }
+    }
+});

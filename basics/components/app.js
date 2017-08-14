57c3f3df887d653e8a13c556d157bda41585060c
greeflas <vldmr.kuprienko@gmail.com>
2017-08-14 15:52:08 +0300

    Components: props validation

--- basics/components/app.js
+++ basics/components/app.js
@@ -7,6 +7,42 @@ Vue.component('render-title', {
     template: '<h1>{{ content }}</h1>'
 });
 
+/*
+types:
+String
+Number
+Boolean
+Function
+Object
+Array
+Symbol
+*/
+Vue.component('validation-component', {
+    props: {
+        propA: 'Number',
+        propB: ['String', 'Number'],
+        propC: {
+            type: 'String',
+            required: true
+        },
+        propD: {
+            type: 'Number',
+            default: 100
+        },
+        propE: {
+            type: 'Object',
+            default: function () {
+                return {message: 'not set'};
+            }
+        },
+        propF: {
+            validator: function (value) {
+                return value > 10;
+            }
+        }
+    }
+});
+
 var vm = new Vue({
     el: '#app',
     components: {
